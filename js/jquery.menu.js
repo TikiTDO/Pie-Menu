300cec387e78cf4221fd6a4f6755dc3afcf24892
TikiTDO <TikiTDO@gmail.com>
2015-04-10 19:03:19 -0400

    Update jquery.menu.js

--- js/jquery.menu.js
+++ js/jquery.menu.js
@@ -8,18 +8,23 @@
           starting_angel: '0',
           angel_difference: '90',
           radius: '200',
-          menu_elements: $(this).children('.menu_option').children(),
-          menu_button : $(this).children('.menu_button'),
         }, options);
 
+      // DOM Elements
+      var menu_container = $(this),
+          menu_elements = $(this).children('.menu_option').children(),
+          menu_button = $(this).children('.menu_button');
+
+      // Add the element to the list
+
       // Local Arguments
-      var angle_between_elements = parseInt(settings.angel_difference)/(settings.menu_elements.length-1),
-        menu_element_angle = [],
-        x_pos = [],
-        y_pos = [];
+      var angle_between_elements = parseInt(settings.angel_difference)/(menu_elements.length-1),
+          menu_element_angle = [],
+          x_pos = [],
+          y_pos = [];
 
       // Initial Values
-      settings.menu_elements.each(function(i, raw_element) {
+      menu_elements.each(function(i, raw_element) {
         var menu_element = $(raw_element);
 
         // Calculate the button positions
@@ -27,21 +32,21 @@
         x_pos[i] = (parseInt(settings.radius) * Math.sin(menu_element_angle[i]));
         y_pos[i] = (parseInt(settings.radius) * Math.cos(menu_element_angle[i]));
          
-        // Calculatate 
-        var rotate_transform = 'rotate('+(90-menu_element_angle[i]*180/Math.PI)+'deg)';
-        menu_element.css({
-          '-webkit-transform': rotate_transform,
-          '-moz-transform': rotate_transform,
-          '-ms-transform': rotate_transform,
-          '-o-transform': rotate_transform,
-          'transform': rotate_transform
-        });
+        // Calculatate Rotation
+        // var rotate_transform = 'rotate('+(90-menu_element_angle[i]*180/Math.PI)+'deg)';
+        // menu_element.css({
+        //   '-webkit-transform': rotate_transform,
+        //   '-moz-transform': rotate_transform,
+        //   '-ms-transform': rotate_transform,
+        //   '-o-transform': rotate_transform,
+        //   'transform': rotate_transform
+        // });
       });
 
       //// Helper Functions
       // Sets the menu position to either the origin, or the calculated position
       function setPosition(origin){
-        $(settings.menu_elements).each(function(i, raw_element){
+        $(menu_elements).each(function(i, raw_element){
           var menu_element = $(raw_element);
 
           // Show the menu elements before opening them
@@ -62,28 +67,36 @@
       }
       
       //// Menu Operation
-      var clickHandler = function() {
+      function clickHandler() {
         var button = $(this),
             button_container = button.parent();
 
-
         if(button_container.hasClass('active')) {
+          // Remove the close event handler
+          menu_elements.off('click.PieMenu', 'a');
+
           // Close the menu
           setPosition(true);
           button_container.removeClass('active').addClass('inactive');;
         } else {
+          // Close other menus
+          $('.active > .menu_button').click();
+
           // Open the menu
           setPosition(false);
           button_container.removeClass('inactive').addClass('active');
-        }
 
-        // Rotate the button
-        $(this).toggleClass("btn-rotate");
+          // Close the menu after clicking an option, unless it specifies data-keep-open
+          menu_elements.on('click.PieMenu', 'a', function () {
+              if($(this).data('keep-open') != undefined) return;
+              button.click();
+            });
+        }
       };
 
       // Ensure click handler is configured
-      $(settings.menu_button).unbind('click', clickHandler);  // I don't think this will work
-      $(settings.menu_button).bind('click', clickHandler);
+      $(menu_button).unbind('click', clickHandler);  // I don't think this will work
+      $(menu_button).bind('click', clickHandler);
     });
   };
 })( jQuery );
